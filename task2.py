365c9dc22b10a9cb2a718204571cf33a49f011a9
DaZorig <98804139+DaZorig@users.noreply.github.com>
2025-04-13 11:17:21 +0300

    Add files via upload

--- task2.py
+++ task2.py
@@ -6,17 +6,17 @@ spark = SparkSession.builder.appName("Product_Category").getOrCreate()
 
 # Создадим датафрейм products (продукты)
 products = spark.createDataFrame([
-    {"productId": 1, "productName": "Продукт 1"},
-    {"productId": 2, "productName": "Продукт 2"},
-    {"productId": 3, "productName": "Продукт 3"},
-    {"productId": 4, "productName": "Продукт 4"},
+    {"productId": 1, "productName": "Product A"},
+    {"productId": 2, "productName": "Product B"},
+    {"productId": 3, "productName": "Product C"},
+    {"productId": 4, "productName": "Product D"},
 ])
 
 # Создадим датафрейм categories (категории)
 categories = spark.createDataFrame([
-    {"categoryId": 1, "categoryName": "Категория 1"},
-    {"categoryId": 2, "categoryName": "Категория 2"},
-    {"categoryId": 3, "categoryName": "Категория 3"},
+    {"categoryId": 1, "categoryName": "Category X"},
+    {"categoryId": 2, "categoryName": "Category Y"},
+    {"categoryId": 3, "categoryName": "Category Z"},
 ])
 
 # Создадим датафрейм product_categories (связи между продуктами и категориями)
@@ -30,10 +30,41 @@ product_categories = spark.createDataFrame([
 # Создадим и отобразим представление для каждого датафрейма
 products.createOrReplaceTempView("products")
 products.show()
+
+# Products
+# ---------+-----------+
+# |productId|productName|
+# +---------+-----------+
+# |        1|  Product A|
+# |        2|  Product B|
+# |        3|  Product C|
+# |        4|  Product D|
+# +---------+-----------+
+
 categories.createOrReplaceTempView("categories")
 categories.show()
+
+# Categories
+# +----------+------------+
+# |categoryId|categoryName|
+# +----------+------------+
+# |         1|  Category X|
+# |         2|  Category Y|
+# |         3|  Category Z|
+# +----------+------------+
+
 product_categories.createOrReplaceTempView("product_categories")
 product_categories.show()
+
+# Product_categories
+# +----------+---------+
+# |categoryId|productId|
+# +----------+---------+
+# |         1|        1|
+# |         2|        1|
+# |         2|        2|
+# |         3|        3|
+# +----------+---------+
 
 # Напишем SQL-запрос для создания нового датафрейма final_df
 final_df = spark.sql("""
@@ -45,3 +76,13 @@ final_df = spark.sql("""
 
 # Отобразим конечный датафрейм
 final_df.show()
+
+# +-----------+------------+
+# |productName|categoryName|
+# +-----------+------------+
+# |  Product D|        null|
+# |  Product A|  Category X|
+# |  Product C|  Category Z|
+# |  Product A|  Category Y|
+# |  Product B|  Category Y|
+# +-----------+------------+
